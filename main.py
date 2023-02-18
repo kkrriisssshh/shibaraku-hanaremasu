1309cf740b985e65c5c76648f9bf5a9ec177e3fd
kkrriisssshh <jamunasittampalam@gmail.com>
2023-02-18 19:36:43 +0100

    v1.5

--- main.py
+++ main.py
@@ -1,15 +1,18 @@
 import psutil
 import time
 import random
+import win32con
 import win32api
+import ctypes
+import ctypes.wintypes
 import toml
 import os
 
 # define the default key assignments
 DEFAULT_KEYS = {
-    "forward": "w",
-    "backward": "s",
-    "left": "q",
+    "forward": "z",
+    "backward": "q",
+    "left": "s",
     "right": "d"
 }
 
@@ -29,6 +32,24 @@ for key, letter in config["keys"].items():
 
 # get the current time
 last_interaction_time = time.time()
+
+# define the callback function for the keyboard hook
+def keyboard_callback(nCode, wParam, lParam):
+    global last_interaction_time
+    # check if the user pressed a key
+    if wParam == win32con.WM_KEYDOWN:
+        # translate the VK code to a key name
+        key_name = win32api.GetKeyNameText(lParam[0], 0)
+        print("Key pressed:", key_name)
+        # update the last interaction time
+        last_interaction_time = win32api.GetLastInputInfo() / 1000
+    return ctypes.windll.user32.CallNextHookEx(None, nCode, wParam, lParam)
+
+# create the C function pointer for the keyboard hook
+keyboard_hook_callback = ctypes.WINFUNCTYPE(ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.POINTER(ctypes.c_void_p))(keyboard_callback)
+
+# create the keyboard hook
+keyboard_hook = ctypes.windll.user32.SetWindowsHookExW(win32con.WH_KEYBOARD_LL, keyboard_hook_callback, ctypes.windll.kernel32.GetModuleHandleW(None), 0)
 
 for p in psutil.process_iter(attrs=['pid', 'name']):
     if p.info['name'] == "VALORANT.exe":
