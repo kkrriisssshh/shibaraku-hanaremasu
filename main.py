cbfc05c5fe5ef5d131362153694c36e785730dc5
kkrriisssshh <jamunasittampalam@gmail.com>
2023-02-18 19:29:56 +0100

    user-asignable keys

--- main.py
+++ main.py
@@ -2,12 +2,36 @@ import psutil
 import time
 import random
 import win32api
+import toml
+import os
 
-keys = [0x5A, 0x51, 0x53, 0x44] # define a list of keys to simulate keypress
-last_interaction_time = time.time() # get the current time
+# define the default key assignments
+DEFAULT_KEYS = {
+    "forward": "w",
+    "backward": "s",
+    "left": "q",
+    "right": "d"
+}
 
-for p in psutil.process_iter(attrs=['pid', 'name']): 
-    if p.info['name'] == "VALORANT.exe": 
+# create the default config file if it doesn't exist
+if not os.path.exists("config.toml"):
+    with open("config.toml", "w") as f:
+        toml.dump({"keys": DEFAULT_KEYS}, f)
+
+# load the config file
+config = toml.load("config.toml")
+
+# translate the letter keys to their VK codes
+key_map = {}
+for key, letter in config["keys"].items():
+    vk_code = win32api.VkKeyScan(letter)
+    key_map[key] = vk_code
+
+# get the current time
+last_interaction_time = time.time()
+
+for p in psutil.process_iter(attrs=['pid', 'name']):
+    if p.info['name'] == "VALORANT.exe":
         print((p.info['name']).upper(), "IS RUNNING!")
         while True:
             # check if the user has interacted with the keyboard
@@ -15,13 +39,11 @@ for p in psutil.process_iter(attrs=['pid', 'name']):
                 # if no interaction is detected for 4 minutes, simulate keypress at random intervals
                 for i in range(5):
                     time.sleep(random.uniform(0.1, 0.5))
-                    win32api.keybd_event(random.choice(keys), 0, 0, 0)
-                    win32api.keybd_event(random.choice(keys), 0, win32api.KEYEVENTF_KEYUP, 0)
+                    win32api.keybd_event(key_map[random.choice(["forward", "backward", "left", "right"])], 0, 0, 0)
+                    win32api.keybd_event(key_map[random.choice(["forward", "backward", "left", "right"])], 0, win32api.KEYEVENTF_KEYUP, 0)
                 # update the last interaction time
                 last_interaction_time = win32api.GetLastInputInfo() / 1000
             else:
                 # if the user has interacted with the keyboard, update the last interaction time
                 last_interaction_time = win32api.GetLastInputInfo() / 1000
             time.sleep(1)  # wait for 1 second before checking again
-    else:
-        print((p.info['name']).upper(), "IS NOT RUNNING!")
